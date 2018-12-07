8f09cd9ce19998f645c654380a67eac3ce5f14ac
Christopher Watson <christopher_watson@icloud.com>
2018-12-07 17:00:13 -0500

    file struc

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Home from "./containers/Home";
+// import Login from "./containers/Login";
+// import Logout from "./containers/Logout";
+import "./App.css";
+
+const App = () => (
+  <Router>
+    <div className='wrapper'>
+      {/* <Navbar /> */}
+      <Switch>
+        {/* <Route exact path="/" component={Home} /> */}
+        <Route render={() => <h1 className="text-center mt-5">Page Not Found!</h1>} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
